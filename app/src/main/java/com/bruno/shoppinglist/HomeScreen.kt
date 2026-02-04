02ed7ece43c7c0b05aa4ab9df39ed572593905a9
BrunoPascoa76 <brunopascoa03@ua.pt>
2026-02-04 14:26:31 +0000

    fixing warnings

--- app/src/main/java/com/bruno/shoppinglist/HomeScreen.kt
+++ app/src/main/java/com/bruno/shoppinglist/HomeScreen.kt
@@ -2,8 +2,6 @@ package com.bruno.shoppinglist
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 
 @Composable
